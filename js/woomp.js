4fb3791d764ce6f2add6a0468822a1a65ef6726c
John Eargle <jeargle@gmail.com>
2019-03-02 17:03:19 -0600

    #9 removed semicolons

--- js/woomp.js
+++ js/woomp.js
@@ -1,26 +1,26 @@
-var score, bootScene, loadScene, titleScene, playScene, endScene, game;
+var score, bootScene, loadScene, titleScene, playScene, endScene, game
 
-score = 0;
+score = 0
 
 bootScene = {
     key: 'boot',
     active: true,
     init: (config) => {
-        console.log('[BOOT] init', config);
+        console.log('[BOOT] init', config)
     },
     preload: () => {
-        console.log('[BOOT] preload');
+        console.log('[BOOT] preload')
     },
     create: function() {
-        'use strict';
+        'use strict'
 
-        game.scene.start('load');
-        game.scene.remove('boot');
+        game.scene.start('load')
+        game.scene.remove('boot')
     },
     update: () => {
-        console.log('[BOOT] update');
+        console.log('[BOOT] update')
     }
-};
+}
 
 loadScene = {
     key: 'load',
@@ -30,126 +30,126 @@ loadScene = {
     width: 320,
     height: 200,
     init: (config) => {
-        console.log('[LOAD] init', config);
+        console.log('[LOAD] init', config)
     },
     preload: function() {
-        'use strict';
-        var loadLbl;
+        'use strict'
+        var loadLbl
 
         loadLbl = this.add.text(80, 160, 'loading...',
                                 {font: '30px Courier',
-                                 fill: '#ffffff'});
+                                 fill: '#ffffff'})
 
         // Load images
-        this.load.image('player', 'assets/circle-blue.png');
-        this.load.image('enemy', 'assets/circle-red.png');
-        this.load.image('platform', 'assets/square-green.png');
+        this.load.image('player', 'assets/circle-blue.png')
+        this.load.image('enemy', 'assets/circle-red.png')
+        this.load.image('platform', 'assets/square-green.png')
 
         // Load sound effects
     },
     create: function() {
-        'use strict';
-        game.scene.start('title');
-        game.scene.remove('load');
+        'use strict'
+        game.scene.start('title')
+        game.scene.remove('load')
     },
     update: () => {
-        console.log('[LOAD] update');
+        console.log('[LOAD] update')
     }
-};
+}
 
 titleScene = {
     key: 'title',
     init: (config) => {
-        console.log('[TITLE] init', config);
+        console.log('[TITLE] init', config)
     },
     preload: () => {
-        console.log('[TITLE] preload');
+        console.log('[TITLE] preload')
     },
     create: function() {
-        'use strict';
-        var nameLbl, startLbl;
+        'use strict'
+        var nameLbl, startLbl
 
         nameLbl = this.add.text(80, 160, 'WOOMP',
                                 {font: '50px Courier',
-                                 fill: '#ffffff'});
+                                 fill: '#ffffff'})
         startLbl = this.add.text(80, 240, 'press "E" to start',
                                  {font: '30px Courier',
-                                  fill: '#ffffff'});
+                                  fill: '#ffffff'})
 
-        this.input.keyboard.on('keydown-E', this.play, this);
+        this.input.keyboard.on('keydown-E', this.play, this)
     },
     update: () => {
-        console.log('[TITLE] update');
+        console.log('[TITLE] update')
     },
     extend: {
         play: function() {
-            'use strict';
-            console.log('[TITLE] play');
-            game.scene.switch('title', 'play');
+            'use strict'
+            console.log('[TITLE] play')
+            game.scene.switch('title', 'play')
         }
     }
-};
+}
 
 playScene = {
     key: 'play',
     create: function() {
-        'use strict';
-        var block, height, width;
+        'use strict'
+        var block, height, width
 
-        console.log('[PLAY] create');
+        console.log('[PLAY] create')
 
         // Walls
-        this.walls = this.physics.add.staticGroup();
+        this.walls = this.physics.add.staticGroup()
 
-        height = 600;
-        width = 800;
+        height = 600
+        width = 800
         block = this.walls.create(0, height - 32, 'platform')
             .setOrigin(0, 0)
             .setScale(25, 1)
-            .refreshBody();
+            .refreshBody()
 
         block = this.walls.create(0, 0, 'platform')
             .setOrigin(0, 0)
             .setScale(25, 1)
-            .refreshBody();
+            .refreshBody()
 
         block = this.walls.create(0, 32, 'platform')
             .setOrigin(0, 0)
             .setScale(1, 17)
-            .refreshBody();
+            .refreshBody()
 
         block = this.walls.create(width - 32, 32, 'platform')
             .setOrigin(0, 0)
             .setScale(1, 17)
-            .refreshBody();
+            .refreshBody()
 
         block = this.walls.create(200, 200, 'platform')
             .setOrigin(0, 0)
             .setScale(8, 1)
-            .refreshBody();
+            .refreshBody()
 
         block = this.walls.create(400, 400, 'platform')
             .setOrigin(0, 0)
             .setScale(8, 1)
-            .refreshBody();
+            .refreshBody()
 
         // Player
-        this.player = this.physics.add.sprite(150, 150, 'player');
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-        this.playerSpeed = 300;
+        this.player = this.physics.add.sprite(150, 150, 'player')
+        this.player.setBounce(0.2)
+        this.player.setCollideWorldBounds(true)
+        this.playerSpeed = 300
 
-        this.physics.add.collider(this.player, this.walls);
+        this.physics.add.collider(this.player, this.walls)
 
         // Enemies
-        this.enemies = this.physics.add.group();
-        this.enemiesKilled = 0;
-        this.enemySpeed = 80;
-        this.createEnemies();
+        this.enemies = this.physics.add.group()
+        this.enemiesKilled = 0
+        this.enemySpeed = 80
+        this.createEnemies()
 
         // Woomp
-        this.woompTime = 0;
-        this.woompTimeOffset = 300;
+        this.woompTime = 0
+        this.woompTimeOffset = 300
 
         // Controls
         this.cursors = this.input.keyboard.addKeys({
@@ -158,147 +158,158 @@ playScene = {
             'left': Phaser.Input.Keyboard.KeyCodes.A,
             'right': Phaser.Input.Keyboard.KeyCodes.D,
             'woomp': Phaser.Input.Keyboard.KeyCodes.SPACEBAR
-        });
+        })
 
-        this.physics.add.collider(this.enemies, this.walls);
-        this.physics.add.collider(this.enemies, this.enemies);
+        this.physics.add.collider(this.enemies, this.walls)
+        this.physics.add.collider(this.enemies, this.enemies)
         this.physics.add.overlap(this.player, this.enemies,
-                                 this.end, null, this);
+                                 this.end, null, this)
 
     },
     update: function() {
-        'use strict';
-        var enemy, that, now;
+        'use strict'
+        var enemy, that, now
 
-        // console.log('[PLAY] update');
+        // console.log('[PLAY] update')
 
         // Update player.
-        this.player.body.setVelocityX(0);
-        this.player.body.setVelocityY(0);
+        this.player.body.setVelocityX(0)
+        this.player.body.setVelocityY(0)
         if (this.cursors.right.isDown) {
-            console.log('RIGHT');
-            this.player.body.setVelocityX(this.playerSpeed);
+            console.log('RIGHT')
+            this.player.body.setVelocityX(this.playerSpeed)
         }
         else if (this.cursors.left.isDown) {
-            console.log('LEFT');
-            this.player.body.setVelocityX(-this.playerSpeed);
+            console.log('LEFT')
+            this.player.body.setVelocityX(-this.playerSpeed)
         }
         else if (this.cursors.up.isDown) {
-            console.log('UP');
-            this.player.body.setVelocityY(-this.playerSpeed);
+            console.log('UP')
+            this.player.body.setVelocityY(-this.playerSpeed)
         }
         else if (this.cursors.down.isDown) {
-            console.log('DOWN');
-            this.player.body.setVelocityY(this.playerSpeed);
+            console.log('DOWN')
+            this.player.body.setVelocityY(this.playerSpeed)
         }
 
         if (this.cursors.woomp.isDown) {
-            this.woomp();
+            this.woomp()
         }
 
         // Update enemies.
         // Also could use this.enemies.children (array of objects).
-        that = this;
+        that = this
         this.enemies.children.iterate(function(enemy) {
-            now = that.time.now;
+            now = that.time.now
             if ((Math.abs(enemy.body.velocity.x) < that.enemySpeed &&
                  Math.abs(enemy.body.velocity.y) < that.enemySpeed) ||
                 now > enemy.moveTime) {
-                enemy.body.velocity.x = 0;
-                enemy.body.velocity.y = 0;
+                enemy.body.velocity.x = 0
+                enemy.body.velocity.y = 0
                 switch(Phaser.Math.RND.integerInRange(0, 3)) {
                 case 0:
-                    enemy.body.velocity.x = that.enemySpeed;
-                    break;
+                    enemy.body.velocity.x = that.enemySpeed
+                    break
                 case 1:
-                    enemy.body.velocity.y = that.enemySpeed;
-                    break;
+                    enemy.body.velocity.y = that.enemySpeed
+                    break
                 case 2:
-                    enemy.body.velocity.x = -that.enemySpeed;
-                    break;
+                    enemy.body.velocity.x = -that.enemySpeed
+                    break
                 case 3:
-                    enemy.body.velocity.y = -that.enemySpeed;
-                    break;
+                    enemy.body.velocity.y = -that.enemySpeed
+                    break
                 }
-                enemy.moveTime = now + Phaser.Math.RND.integerInRange(5, 10)*200;
+                enemy.moveTime = now + Phaser.Math.RND.integerInRange(5, 10)*200
             }
-        });
+        })
     },
     extend: {
         woomp: function() {
-            'use strict';
-            var now;
+            'use strict'
+            var now
 
-            now = this.time.now;
+            now = this.time.now
 
             if (now > this.woompTime) {
-                console.log('woomp');
-                this.woompTime = now + this.woompTimeOffset;
+                console.log('woomp')
+                this.woompTime = now + this.woompTimeOffset
             }
         },
         createEnemies: function() {
-            'use strict';
-            var i, positions, pos, enemy;
+            'use strict'
+            var i, positions, pos, enemy
 
-            console.log('createEnemies()');
+            console.log('createEnemies()')
 
             positions = [
                 [50, 50],
                 [150, 100],
                 [250, 50],
                 [350, 100]
-            ];
+            ]
             for (i=0; i<positions.length; i++) {
-                pos = positions[i];
-                enemy = this.enemies.create(pos[0], pos[1], 'enemy');
+                pos = positions[i]
+                enemy = this.enemies.create(pos[0], pos[1], 'enemy')
             }
         },
         die: function(player, enemy) {
-            'use strict';
+            'use strict'
 
-            console.log('die');
+            console.log('die')
 
-            this.end();
+            this.end()
         },
         end: function() {
-            'use strict';
-            console.log('[PLAY] end');
-            this.scene.restart();
+            'use strict'
+            console.log('[PLAY] end')
+            this.registry.destroy()
+            this.events.off()
             game.scene.switch('play', 'end')
-            this.cursors.right.isDown = false;
-            this.cursors.left.isDown = false;
-            this.cursors.up.isDown = false;
-            this.cursors.down.isDown = false;
-            console.log('CURSORS OFF');
+            this.cursors.right.isDown = false
+            this.cursors.left.isDown = false
+            this.cursors.up.isDown = false
+            this.cursors.down.isDown = false
+            console.log('[PLAY] CURSORS OFF')
+            this.scene.stop()
         }
     }
-};
+}
 
 endScene = {
     key: 'end',
     create: function() {
-        'use strict';
-        var scoreLbl, nameLbl, startLbl;
+        'use strict'
+        var scoreLbl, nameLbl, startLbl
+
+        console.log('[END] create')
 
         scoreLbl = this.add.text(600, 10, 'Score: ' + score,
                                  {font: '30px Courier',
-                                  fill: '#ffffff'});
+                                  fill: '#ffffff'})
         nameLbl = this.add.text(80, 160, 'YOU DIED',
                                 {font: '50px Courier',
-                                 fill: '#ffffff'});
+                                 fill: '#ffffff'})
         startLbl = this.add.text(80, 240, 'press "W" to restart',
                                  {font: '30px Courier',
-                                  fill: '#ffffff'});
+                                  fill: '#ffffff'})
 
-        this.input.keyboard.on('keydown-W', this.restart, this);
+        this.input.keyboard.on('keydown-W', this.restart, this)
     },
+    // update: function() {
+    //     'use strict'
+    //     var enemy, that, now
+
+    //     console.log('[END] update')
+    // },
     extend: {
         restart: function() {
-            'use strict';
+            'use strict'
+            console.log('[END] restart')
             game.scene.switch('end', 'title')
         }
     }
-};
+}
 
 
 const gameConfig = {
@@ -314,7 +325,7 @@ const gameConfig = {
         }
     },
     scene: [ bootScene, loadScene, titleScene, playScene, endScene ]
-};
+}
 
-game = new Phaser.Game(gameConfig);
-game.scene.start('boot', { someData: '...arbitrary data' });
+game = new Phaser.Game(gameConfig)
+game.scene.start('boot', { someData: '...arbitrary data' })
